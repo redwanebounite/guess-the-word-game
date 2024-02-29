55cc87cb24fb3911188b1ba059ba742665063035
redwane <bounitredwane@gmail.com>
2024-02-29 22:50:46 +0100

    added js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let numberLetters = 6;
 let currentTry = 1;
 
 let wordToGuess = "";
+let messageArea = document.querySelector(".message")
+
 function generateInput() {
     let inputContainer = document.querySelector(".inputs")
     for (let i = 1; i <= numberTries ; i++){
@@ -37,20 +39,51 @@ function generateInput() {
                     nextInput.focus(); 
                 }
             });
+            input.addEventListener("keydown" , function (event){
+                let currentIndex = Array.from(inputs).indexOf(event.target);
+                if (event.key === "ArrowRight"){
+                    let nextInput = currentIndex + 1;
+                    if (nextInput < inputs.length) inputs[nextInput].focus();
+                }
+                if (event.key === "ArrowLeft"){
+                    let prevtInput = currentIndex - 1;
+                    if (prevtInput >= 0) inputs[prevtInput].focus();
+                }
+            })
         })
-
+        console.log(wordToGuess);
     }
 }
 let guessBtn = document.querySelector(".check");
+guessBtn.addEventListener("click",handleGuesses);
 let words = ["Create","Update","Delete","Master","Branch","Mainly"];
-wordToGuess = words[Math.floor(Math.random()*words.length)].toLocaleLowerCase();
+wordToGuess = words[Math.floor(Math.random()*words.length)].toLowerCase();
 
 function handleGuesses() {
     let successGuess = true;
-    for (let i=1; i<= numberLetters; i++ ){
+    for (let i=1; i<= numberLetters ; i++ ){
         let inputField = document.querySelector(`#guess-${currentTry}-letter-${i}`);
-        let letter = inputField.value.toLocaleLowerCase();
+        let letter = inputField.value.toLowerCase();
         let actualLetter = wordToGuess[i -1];
+
+        if (letter === actualLetter) {
+            inputField.classList.add("in-place") 
+        } else if (wordToGuess.includes(letter) && letter !== ""){
+            inputField.classList.add("not-in-place")
+            successGuess = false
+        }else {
+            inputField.classList.add("no");
+            successGuess = false;
+        }
+    }
+    if (successGuess) {
+        messageArea.innerHTML = `You Win The Word Is <span>${wordToGuess}</span>`
+        let allTries = document.querySelectorAll(".inputs > div");
+        allTries.forEach((tryDiv) => tryDiv.classList.add("disabled-inputs"));
+        guessBtn.style.backgroundColor = "#27303f"
+        guessBtn.style.cursor = "no-drop"
+    }else {
+
     }
 }
 
